998227d843547ea0c86e6b72966ad0fe124fd655
gurgenmosesyan <gurgen.mosesyan@gmail.com>
2016-08-26 18:05:46 +0400

    commit

--- resources/lang/en/www.php
+++ resources/lang/en/www.php
@@ -8,4 +8,13 @@ return array (
   'base.label.brands' => 'Brands',
   'base.label.ads' => 'Ads',
   'home.top.title' => 'Even <span class="yel">Advertising</span> Needs an Advertising',
+  'home.top.sub_title' => 'Here you can find advertisings, brands from all over the world and find out the story about them. You can also know people working on them.',
+  'home.main.title' => 'Some text title here',
+  'home.main.sub_title' => 'Here you can find advertisings, brands from all over the world and find out the story about them. You can also know people working on them.',
+  'home.main.title2' => 'We can help you to:',
+  'home.main.text1' => 'Place your ads',
+  'home.main.text2' => 'Find Brands & Agencies',
+  'home.main.text3' => 'Create profile & find job',
+  'btn.see_all_ads' => 'See all ads',
+  'home.main.social.title' => 'See our social pages',
 );
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -12,10 +12,39 @@ $title = trans('www.homepage.title');
 
 <div id="home-top" class="tc">
     <div class="overlay">
-        <h1 class="fb">{!!trans('www.home.top.title')!!}</h1>
+        <div class="titles">
+            <h1 class="fb fs34">{!!trans('www.home.top.title')!!}</h1>
+            <p class="light fs20">{{trans('www.home.top.sub_title')}}</p>
+        </div>
     </div>
 </div>
 
-<div id="home-main"></div>
+<div id="home-main">
+    <div class="bg-right"></div>
+    <div class="dot-bg">
+        <div class="page">
+            <div class="fl bg-ads"></div>
+            <div class="fl info-box">
+                <h2 class="fb fs40">{{trans('www.home.main.title')}}</h2>
+                <p class="sub-title fs20">{{trans('www.home.main.sub_title')}}</p>
+                <h3 class="fb fs22">{{trans('www.home.main.title2')}}</h3>
+                <p class="fb info fs20">{{trans('www.home.main.text1')}}</p>
+                <p class="fb info fs20">{{trans('www.home.main.text2')}}</p>
+                <p class="fb info fs20">{{trans('www.home.main.text3')}}</p>
+                <div class="btn-box">
+                    <a href="" class="btn-black">{{trans('www.btn.see_all_ads')}}</a>
+                </div>
+                <div class="social-box">
+                    <div class="fl social-title fs20">{{trans('www.home.main.social.title')}}</div>
+                    <a href="{{trans('www.social.link.fb')}}" class="facebook social db fl" target="_blank"></a>
+                    <a href="{{trans('www.social.link.twitter')}}" class="twitter social db fl" target="_blank"></a>
+                    <a href="{{trans('www.social.link.google')}}" class="google social db fl" target="_blank"></a>
+                    <a href="{{trans('www.social.link.youtube')}}" class="youtube social db fl" target="_blank"></a>
+                </div>
+            </div>
+            <div class="cb"></div>
+        </div>
+    </div>
+</div>
 
 @stop
